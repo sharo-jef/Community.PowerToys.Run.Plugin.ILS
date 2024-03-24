a2fae10644fe828c298f8265f8202cff38b23c15
sharo-jef <sharo.jef@gmail.com>
2024-03-24 22:18:06 +0900

    add docker build

--- Build-Solution.ps1
+++ Build-Solution.ps1
@@ -1,3 +1,7 @@
+Param(
+  [switch]$Docker = $False
+)
+
 $ErrorActionPreference = "Stop"
 
 $Version = (Get-Content -Raw -Path "$PSScriptRoot\ILS\plugin.json" | ConvertFrom-Json).Version
@@ -6,13 +10,17 @@ if (Test-Path -Path "$PSScriptRoot\ILS\bin") {
   Remove-Item -Path "$PSScriptRoot\ILS\bin\*" -Recurse
 }
 
-dotnet build $PSScriptRoot\ILS.sln -c Release /p:Platform="Any CPU"
-# docker run -v "${PSScriptRoot}:/app" -w /app mcr.microsoft.com/dotnet/sdk:8.0 dotnet build ILS.sln -c Release /p:Platform="Any CPU"
+if ($Docker) {
+  docker run -v "${PSScriptRoot}:/app" -w /app mcr.microsoft.com/dotnet/sdk:8.0 dotnet build ILS.sln -c Release /p:Platform="Any CPU"
+}
+else {
+  dotnet build $PSScriptRoot\ILS.sln -c Release /p:Platform="Any CPU"
+}
 
-Remove-Item -Path "$PSScriptRoot\ILS\bin\*" -Recurse -Include *.xml, *.pdb, PowerToys.*, Wox.*
+Remove-Item -Path "$PSScriptRoot\ILS\bin\*" -Recurse -Include *.xml, *.pdb, PowerToys.*, Wox.* -ErrorAction SilentlyContinue
 Rename-Item -Path "$PSScriptRoot\ILS\bin\Release" -NewName "ILS"
 
-if (Test-Path -Path $PSScriptRoot\ILS-$Version.zip) {
-  Remove-Item -Path $PSScriptRoot\ILS-$Version.zip
+if (Test-Path -Path "$PSScriptRoot\ILS-$Version.zip") {
+  Remove-Item -Path "$PSScriptRoot\ILS-$Version.zip"
 }
 Compress-Archive -Path "$PSScriptRoot\ILS\bin\ILS\net6.0-windows\*" -DestinationPath "$PSScriptRoot\ILS-$Version.zip"
